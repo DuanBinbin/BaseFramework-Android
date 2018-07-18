68da379aefa1498798c0e5d53f0551cb6a575862
duan bin <binbin.duan@pisofttech.com>
2018-07-18 19:49:33 +0800

    配置Gradle的引用

--- app/src/main/java/com/db/bfa/MainActivity.java
+++ app/src/main/java/com/db/bfa/MainActivity.java
@@ -3,11 +3,17 @@ package com.db.bfa;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.db.basic.Singleton.EasySingleton;
+
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void testSingletonPattern(){
+        EasySingleton.INSTANCE.whateverMethod();
     }
 }
